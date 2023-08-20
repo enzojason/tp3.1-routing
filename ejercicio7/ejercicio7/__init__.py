1f7ee4334e42dcbdb964a254a0d20a0e5bf066cf
NoeliaTG <122331273+NoeliaTG@users.noreply.github.com>
2023-08-19 22:59:20 -0300

    ejercicio 7
    Falta corregir el request

---
+++ ejercicio7/ejercicio7/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from ..config import Config
+def init_app():
+    """Crea y configura la aplicación Flask"""
+    app = Flask(__name__, static_folder = Config.STATIC_FOLDER, template_folder = Config.TEMPLATE_FOLDER)
+    app.config.from_object(Config)
+    # Ruta que recibe parámetros de consulta.
+    @app.route('/operate', methods=['GET'])
+    def operacion_num():
+        operacion = request.args.get('operacion')
+        num1 = init(request.args.get('num1'))
+        num2 = init(request.args.get('num2'))
+        
+        if operacion == 'sum':
+            resultado = num1 + num2
+        elif operacion == 'sub' :
+            resultado = num1 - num2
+        elif operacion == 'mult' :
+            resultado = num1 * num2
+        elif operacion == 'div' :
+            if num2 != 0:
+                resultado = num1 / num2
+            else:
+                return {'La división no está definida para el denominador 0'}
+        else:
+            return f'Operación no válida: {operacion}'
+        return {'resultado': resultado } 
+    return app
